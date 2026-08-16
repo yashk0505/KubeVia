c2804c3d1b2d390cb6c64e2b802e81791e5f1d7d
Yash Kalgude <yashkalgude123@gmail.com>
2026-08-17 00:11:37 +0530

    revert: remove cloudflare specific deployment configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
